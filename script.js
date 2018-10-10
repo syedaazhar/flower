2ef338f62a35351a2d32b6e03f865c9417fb59f4
syedaazhar <syedaazhar19@gmail.com>
2018-10-11 04:33:46 +0500

    flower

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
 // Write all your Javascript here!
 
 function showContant( urlLink) {
-
-  
-    
-
-
     $.ajax({
         url: 'https://techkaro-test.herokuapp.com/api/v1/getdata?lang='+urlLink,
 
         success: function (data) {
+            
             document.querySelector('.main-img').style = `background-image:url(${data.imageURL})`;
             document.querySelector('.flower-detail h4').innerHTML = data.info.heading;
             document.querySelector('.flower-detail p').innerHTML = data.info.description;
             document.querySelector('.sale-contain h4').innerHTML = data.sale.heading;
             document.querySelector('.sale-contain p').innerHTML = data.sale.description;
 
-             let year =new Date(data.date);
-            document.querySelector('.month').innerHTML =parseInt(year.getFullYear())
-            let hearderEl =  document.querySelector('header');
-            hearderEl.innerHTML=
-            `<nav class="navbar navbar-expand-lg navbar-dark bg-dark main-nav">
+            let date =new Date(data.date);
+            var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+            document.querySelector('.full-date').innerHTML=`${months[date.getMonth()]} 
+                                                            ${parseInt(date.getDate())}, 
+                                                            ${parseInt(date.getFullYear())} - 
+                                                            ${parseInt(date.getHours())}h 
+                                                            ${parseInt(date.getMinutes())}m`;
+        
+            let hearderEl =  document.querySelector('.navbar-nav');
+            hearderEl.innerHTML = " ";
+            for(let i=0;i<4;i++){
+
+                hearderEl.innerHTML+=
+            `
                     <li class="nav-item">
-                        <a class="nav-link" href="#">${data.menuOptions[0]}<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">${data.menuOptions[1]}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">${data.menuOptions[2]}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">${data.menuOptions[3]}</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>`
+                        <a class="nav-link" href="#">${data.menuOptions[i]}<span class="sr-only">(current)</span></a>
+                    </li>`
+            }
            
         }
     })
